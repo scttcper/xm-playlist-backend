6aab0972570c247efb7fbbd5a7574f14455d91bd
Scott Cooper <scttcper@gmail.com>
2017-06-14 15:39:33 -0700

    remove

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -15,21 +15,3 @@ module.exports = {
   port: 5000,
   dsn: false,
 };
-
-// module.exports = {
-//   username: 'hon',
-//   database: 'xm',
-//   password: 'oRDERYmPYThyDRAwMARAmIDi',
-//   db: {
-//     host: 'supercop.crpujwt1a7km.us-west-1.rds.amazonaws.com',
-//     dialect: 'postgres',
-//     pool: {
-//       max: 8,
-//       min: 0,
-//       idle: 2000,
-//     },
-//   },
-//   token: 'TREKMEG2HHF0H9GH',
-//   port: 5000,
-//   dsn: false,
-// };
