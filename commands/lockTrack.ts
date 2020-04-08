a98cbbc78184343616e519a23702c2a6d59feda4
Scott Cooper <scttcper@gmail.com>
2020-04-07 23:02:07 -0700

    add lock command

--- commands/lockTrack.ts
+++ commands/lockTrack.ts
@@ -1,8 +1,72 @@
-async function lockTrack(): Promise<void> {
+import { argv } from 'yargs';
+import got from 'got';
 
+import { db } from '../src/db';
+import { getTrack } from '../src/track';
+import { Spotify } from '../frontend/models';
+import { getToken, parseSpotify } from '../src/spotify';
+import { findAndCacheLinks } from '../src/linkfinder';
+
+const trackId = argv.track as string;
+let spotifyId = argv.spotify as string;
+if (spotifyId.includes('open.spotify.com')) {
+  spotifyId = spotifyId.split('/track/')[1];
 }
 
-lockTrack().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+console.log({ spotifyId });
+
+const update = argv.update || false;
+
+async function lockTrack(): Promise<void> {
+  const track = await getTrack(trackId);
+  if (!track) {
+    throw new Error('Track not found');
+  }
+
+  const url = `https://api.spotify.com/v1/tracks/${spotifyId}`;
+  const token = await getToken();
+  const headers = { Authorization: `Bearer ${token}` };
+  const res = await got.get(url, { headers }).json<any>();
+  const s = parseSpotify(res);
+  console.log(s);
+
+  if (update) {
+    await db<Spotify>('spotify')
+      .update({
+        cover: s.cover,
+        spotifyId: s.spotifyId,
+        name: s.spotifyName,
+        previewUrl: s.previewUrl,
+        updatedAt: db.fn.now() as any,
+        lock: true,
+      })
+      .where('trackId', trackId);
+  } else {
+    await db<Spotify>('spotify').insert({
+      trackId,
+      cover: s.cover,
+      spotifyId: s.spotifyId,
+      name: s.spotifyName,
+      previewUrl: s.previewUrl,
+      lock: true,
+    });
+  }
+
+  const spotify = await db<Spotify>('spotify').select().where('trackId', trackId).first();
+
+  // remove incorrect links
+  await db('links').delete().where({ trackId: spotify.trackId }).limit(1);
+
+  await findAndCacheLinks(spotify);
+}
+
+lockTrack()
+  .then(async () => {
+    console.log('success');
+    await db.destroy();
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
