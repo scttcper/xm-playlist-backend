1a7ab6716311dd209aab16476a76ccf82f50d574
Scott Cooper <scttcper@gmail.com>
2020-04-18 19:37:36 -0700

    increase size

--- migrations/20191227164855_init.ts
+++ migrations/20191227164855_init.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
 
   await knex.schema.createTable('track', table => {
-    table.string('id', 24).notNullable().primary();
+    table.string('id', 50).notNullable().primary();
     table.string('name', 200).notNullable();
     table.jsonb('artists').defaultTo('[]').notNullable();
 
@@ -13,8 +13,8 @@ export async function up(knex: Knex): Promise<void> {
 
   await knex.schema.createTable('scrobble', table => {
     table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.string('track_id', 24).notNullable();
-    table.string('channel', 24).notNullable();
+    table.string('track_id', 50).notNullable();
+    table.string('channel', 50).notNullable();
     table.timestamp('start_time').notNullable();
 
     table.foreign('track_id').references('track.id');
@@ -23,8 +23,8 @@ export async function up(knex: Knex): Promise<void> {
   });
 
   await knex.schema.createTable('spotify', table => {
-    table.string('track_id', 24).primary().notNullable();
-    table.string('spotify_id', 24).notNullable();
+    table.string('track_id', 50).primary().notNullable();
+    table.string('spotify_id', 50).notNullable();
     table.string('name', 400).notNullable();
     table.string('cover');
     table.string('preview_url');
@@ -36,7 +36,7 @@ export async function up(knex: Knex): Promise<void> {
   });
 
   await knex.schema.createTable('links', table => {
-    table.string('track_id', 24).primary().notNullable();
+    table.string('track_id', 50).primary().notNullable();
     table.jsonb('links').defaultTo('[]').notNullable();
 
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
