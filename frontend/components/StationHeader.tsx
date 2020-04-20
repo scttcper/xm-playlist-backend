7aa44227304bf9d29657d22012be7c08a19d5163
Scott Cooper <scttcper@gmail.com>
2020-04-20 09:19:31 -0700

    fix: increase padding on station image, increase station name and description size

--- frontend/components/StationHeader.tsx
+++ frontend/components/StationHeader.tsx
@@ -5,15 +5,16 @@ import { Channel } from '../channels';
 export const StationHeader: React.FC<{ channel: Channel }> = props => {
   return (
     <div className="media my-2">
-      <img
-        style={{ maxWidth: '140px' }}
-        src={`/static/img/${props.channel.deeplink}-lg.png`}
-        className="img-fluid rounded-lg bg-dark mr-3 p-1"
-        alt="..."
-      />
+      <div className="bg-dark rounded-lg mr-3 p-2" style={{ maxWidth: '140px' }}>
+        <img
+          src={`/static/img/${props.channel.deeplink}-lg.png`}
+          className="img-fluid"
+          alt="..."
+        />
+      </div>
       <div className="media-body align-self-center">
-        <h3 className="mt-0 mb-1">{props.channel.name}</h3>
-        <small>{props.channel.desc}</small>
+        <h2 className="mt-0 mb-1">{props.channel.name}</h2>
+        <p>{props.channel.desc}</p>
       </div>
     </div>
   );
