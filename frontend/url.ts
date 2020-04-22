884e5481afbe668f8e76c3bb5a5d01a0b1b38367
Scott Cooper <scttcper@gmail.com>
2020-04-21 22:52:28 -0700

    fix url

--- frontend/url.ts
+++ frontend/url.ts
@@ -1,3 +1,4 @@
-// const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || '3000';
-export const url = `http://localhost:${port}`;
+
+export const url = dev ? `http://localhost${port}` : 'https://xmplaylist.com';
