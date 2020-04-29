15bbec823efd68689c971aaa6aa407fa20614db6
Scott Cooper <scttcper@gmail.com>
2020-04-28 17:34:16 -0700

    chore: update dependencies

--- frontend/url.ts
+++ frontend/url.ts
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || '3000';
 
-export const url = dev ? `http://localhost${port}` : 'https://xmplaylist.com';
+export const url = dev ? `http://localhost:${port}` : 'https://xmplaylist.com';
