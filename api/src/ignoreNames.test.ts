5e2ed38b70bf6d53d9c639ee4f0741e0c674d185
Scott Cooper <scttcper@gmail.com>
2020-06-16 12:39:03 -0700

    fix paths

--- api/src/ignoreNames.test.ts
+++ api/src/ignoreNames.test.ts
@@ -7,4 +7,9 @@ it('it should block garbage', () => {
   expect(matchesGarbage({ cut: { artists: [{ name: 'New & Trending!' }], title: 'Pandora Now' } } as any)).toBeTruthy();
   expect(matchesGarbage({ cut: { artists: [{ name: 'SoulCycle Radio' }], title: 'on SiriusXM' } } as any)).toBeTruthy();
   expect(matchesGarbage({ cut: { artists: [{ name: 'Send yours!' }], title: 'social@soul-cycle.com' } } as any)).toBeTruthy();
+  expect(matchesGarbage({ cut: { artists: [{ name: 'Be a Hit-Maker!' }], title: 'siriusxm.com/hits1' } } as any)).toBeTruthy();
+});
+
+it('it should allow non garbage', () => {
+  expect(matchesGarbage({ cut: { artists: [{ name: 'Kanye' }], title: 'Follow God' } } as any)).toBeFalsy();
 });
