768708a53eeb11384f9d1bd21cb962a2c7662e12
Scott Cooper <scttcper@gmail.com>
2020-07-18 14:27:44 -0700

    add sentry to server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,11 @@
 import { Server } from '@hapi/hapi';
+import * as Sentry from '@sentry/node';
 
+import config from '../config';
 import { registerApiRoutes } from './apiRoutes';
 
 const port = parseInt(process.env.PORT, 10) || 5000;
+Sentry.init({ dsn: config.dsn });
 
 (async () => {
   const server = new Server({
