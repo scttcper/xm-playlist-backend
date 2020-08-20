4e602ba0a0a1bd89a568a89b9cb0d0c712ae4dc1
Scott Cooper <scttcper@gmail.com>
2020-08-20 00:03:31 -0700

    init fastify

--- api/src/apiRoutes.ts
+++ api/src/apiRoutes.ts
@@ -1,5 +1,4 @@
-import { Server as HapiServer } from '@hapi/hapi';
-import laabr from 'laabr';
+import { FastifyInstance } from 'fastify';
 import Joi from 'joi';
 import Boom from '@hapi/boom';
 import { admin as firebaseAdmin } from 'firebase-admin/lib/auth';
@@ -32,36 +31,37 @@ async function isValidToken(token = ''): Promise<firebaseAdmin.auth.DecodedIdTok
 /**
  * this seems to be required for registering all the nextjs pages right now
  */
-export async function registerApiRoutes(server: HapiServer) {
-  await server.register({
-    plugin: laabr,
-    options: {
-      // formats: { onPostStart: 'log.tiny' },
-    },
-  });
-
-  server.route({
-    path: '/api/station/{id}',
+export async function registerApiRoutes(server: FastifyInstance) {
+  server.route<{ Querystring: { last: string }; Params: { id: string } }>({
     method: 'GET',
-    options: {
-      cache: {
-        privacy: 'public',
-        // 3 min
-        expiresIn: 180000,
-        statuses: [200],
-        otherwise: 'no-cache',
+    url: '/api/station/:id',
+    schema: {
+      querystring: {
+        last: { type: 'string' },
       },
-      cors: { origin: 'ignore' },
-      validate: {
-        params: Joi.object({
-          id: Joi.string().required(),
-        }),
-        query: Joi.object({
-          last: Joi.date().timestamp('javascript'),
-        }),
+      params: {
+        id: { type: 'string' },
       },
     },
-    handler: async req => {
+    // options: {
+    //   cache: {
+    //     privacy: 'public',
+    //     // 3 min
+    //     expiresIn: 180000,
+    //     statuses: [200],
+    //     otherwise: 'no-cache',
+    //   },
+    //   cors: { origin: 'ignore' },
+    //   validate: {
+    //     params: Joi.object({
+    //       id: Joi.string().required(),
+    //     }),
+    //     query: Joi.object({
+    //       last: Joi.date().timestamp('javascript'),
+    //     }),
+    //   },
+    // },
+    handler: async (req, reply) => {
       const channel = getChannel(req.params.id);
 
       const { query } = req;
@@ -73,207 +73,207 @@ export async function registerApiRoutes(server: HapiServer) {
     },
   });
 
-  server.route({
-    path: '/api/station/{id}/newest',
-    method: 'GET',
-    options: {
-      cache: {
-        privacy: 'public',
-        // 10 min
-        expiresIn: 600000,
-        statuses: [200],
-        otherwise: 'no-cache',
-      },
-      cors: { origin: 'ignore' },
-      validate: {
-        params: Joi.object({
-          id: Joi.string().required(),
-        }),
-      },
-    },
-    handler: async req => {
-      const channel = getChannel(req.params.id);
-      return getNewest(channel);
-    },
-  });
+  // server.route({
+  //   path: '/api/station/{id}/newest',
+  //   method: 'GET',
+  //   options: {
+  //     cache: {
+  //       privacy: 'public',
+  //       // 10 min
+  //       expiresIn: 600000,
+  //       statuses: [200],
+  //       otherwise: 'no-cache',
+  //     },
+  //     cors: { origin: 'ignore' },
+  //     validate: {
+  //       params: Joi.object({
+  //         id: Joi.string().required(),
+  //       }),
+  //     },
+  //   },
+  //   handler: async req => {
+  //     const channel = getChannel(req.params.id);
+  //     return getNewest(channel);
+  //   },
+  // });
 
-  server.route({
-    path: '/api/station/{id}/most-heard',
-    method: 'GET',
-    options: {
-      cache: {
-        privacy: 'public',
-        // 10 min
-        expiresIn: 600000,
-        statuses: [200],
-        otherwise: 'no-cache',
-      },
-      cors: { origin: 'ignore' },
-      validate: {
-        params: Joi.object({
-          id: Joi.string().required(),
-        }),
-        query: Joi.object({
-          subDays: Joi.number().optional().default(30).valid(7, 14, 30),
-        }),
-      },
-    },
-    handler: async req => {
-      const channel = getChannel(req.params.id);
-      return getMostHeard(channel, undefined, Number(req.query.subDays));
-    },
-  });
+  // server.route({
+  //   path: '/api/station/{id}/most-heard',
+  //   method: 'GET',
+  //   options: {
+  //     cache: {
+  //       privacy: 'public',
+  //       // 10 min
+  //       expiresIn: 600000,
+  //       statuses: [200],
+  //       otherwise: 'no-cache',
+  //     },
+  //     cors: { origin: 'ignore' },
+  //     validate: {
+  //       params: Joi.object({
+  //         id: Joi.string().required(),
+  //       }),
+  //       query: Joi.object({
+  //         subDays: Joi.number().optional().default(30).valid(7, 14, 30),
+  //       }),
+  //     },
+  //   },
+  //   handler: async req => {
+  //     const channel = getChannel(req.params.id);
+  //     return getMostHeard(channel, undefined, Number(req.query.subDays));
+  //   },
+  // });
 
-  server.route({
-    path: '/api/station/{channelId}/track/{trackId}',
-    method: 'GET',
-    options: {
-      cache: {
-        privacy: 'public',
-        // 10 min * 2
-        expiresIn: 600000,
-        statuses: [200],
-        otherwise: 'no-cache',
-      },
-      cors: { origin: 'ignore' },
-      validate: {
-        params: Joi.object({
-          channelId: Joi.string().required(),
-          trackId: Joi.string().required(),
-        }),
-      },
-    },
-    handler: async req => {
-      const channel = getChannel(req.params.channelId);
-      const [track, plays, recent] = await Promise.all([
-        getTrack(req.params.trackId),
-        getPlays(req.params.trackId, channel),
-        getTrackRecent(req.params.trackId, channel),
-      ]);
+  // server.route({
+  //   path: '/api/station/{channelId}/track/{trackId}',
+  //   method: 'GET',
+  //   options: {
+  //     cache: {
+  //       privacy: 'public',
+  //       // 10 min * 2
+  //       expiresIn: 600000,
+  //       statuses: [200],
+  //       otherwise: 'no-cache',
+  //     },
+  //     cors: { origin: 'ignore' },
+  //     validate: {
+  //       params: Joi.object({
+  //         channelId: Joi.string().required(),
+  //         trackId: Joi.string().required(),
+  //       }),
+  //     },
+  //   },
+  //   handler: async req => {
+  //     const channel = getChannel(req.params.channelId);
+  //     const [track, plays, recent] = await Promise.all([
+  //       getTrack(req.params.trackId),
+  //       getPlays(req.params.trackId, channel),
+  //       getTrackRecent(req.params.trackId, channel),
+  //     ]);
 
-      return { ...track, plays, recent };
-    },
-  });
+  //     return { ...track, plays, recent };
+  //   },
+  // });
 
-  server.route({
-    path: '/api/search',
-    method: 'GET',
-    options: {
-      cors: { origin: 'ignore' },
-      validate: {
-        query: Joi.object({
-          trackName: Joi.string().min(2).max(30).optional(),
-          artistName: Joi.string().min(2).max(30).optional(),
-          station: Joi.string()
-            .optional()
-            .valid(...channels.map(n => n.deeplink)),
-          timeAgo: Joi.number().default(60 * 60 * 24).positive().optional(),
-          currentPage: Joi.number().default(1).positive().optional(),
-        }),
-      },
-    },
-    handler: async req => {
-      let identity: firebaseAdmin.auth.DecodedIdToken;
-      try {
-        identity = await isValidToken(req.headers.authorization);
-      } catch {
-        throw Boom.unauthorized();
-      }
+  // server.route({
+  //   path: '/api/search',
+  //   method: 'GET',
+  //   options: {
+  //     cors: { origin: 'ignore' },
+  //     validate: {
+  //       query: Joi.object({
+  //         trackName: Joi.string().min(2).max(30).optional(),
+  //         artistName: Joi.string().min(2).max(30).optional(),
+  //         station: Joi.string()
+  //           .optional()
+  //           .valid(...channels.map(n => n.deeplink)),
+  //         timeAgo: Joi.number().default(60 * 60 * 24).positive().optional(),
+  //         currentPage: Joi.number().default(1).positive().optional(),
+  //       }),
+  //     },
+  //   },
+  //   handler: async req => {
+  //     let identity: firebaseAdmin.auth.DecodedIdToken;
+  //     try {
+  //       identity = await isValidToken(req.headers.authorization);
+  //     } catch {
+  //       throw Boom.unauthorized();
+  //     }
 
-      const user = await db('user')
-        .select<{ id: string; }>([
-          'user.id as id',
-        ])
-        .where('user.id', '=', identity.uid)
-        .limit(1)
-        .first();
+  //     const user = await db('user')
+  //       .select<{ id: string; }>([
+  //         'user.id as id',
+  //       ])
+  //       .where('user.id', '=', identity.uid)
+  //       .limit(1)
+  //       .first();
 
-      const queryTimeAgo = Number(req.query.timeAgo as string);
-      let timeAgo = queryTimeAgo;
-      let currentPage = Number(req.query.currentPage as string);
-      const maxDays = 30;
-      if (queryTimeAgo > 60 * 60 * 24 * maxDays) {
-        throw Boom.badRequest();
-      }
+  //     const queryTimeAgo = Number(req.query.timeAgo as string);
+  //     let timeAgo = queryTimeAgo;
+  //     let currentPage = Number(req.query.currentPage as string);
+  //     const maxDays = 30;
+  //     if (queryTimeAgo > 60 * 60 * 24 * maxDays) {
+  //       throw Boom.badRequest();
+  //     }
 
-      return search(
-        req.query.trackName as string,
-        req.query.artistName as string,
-        req.query.station as string | undefined,
-        timeAgo,
-        undefined,
-        currentPage,
-      );
-    },
-  });
+  //     return search(
+  //       req.query.trackName as string,
+  //       req.query.artistName as string,
+  //       req.query.station as string | undefined,
+  //       timeAgo,
+  //       undefined,
+  //       currentPage,
+  //     );
+  //   },
+  // });
 
-  server.route({
-    path: '/api/user/{userId}',
-    method: 'GET',
-    options: {
-      cors: { origin: 'ignore' },
-      validate: {
-        params: Joi.object({
-          userId: Joi.string().required(),
-        }),
-      },
-    },
-    handler: async req => {
-      const { userId } = req.params;
-      try {
-        const user = await isValidToken(req.headers.authorization);
-        console.log(userId, user.uid);
-        if (userId !== user.uid) {
-          throw Boom.unauthorized();
-        }
-      } catch {
-        throw Boom.unauthorized();
-      }
+  // server.route({
+  //   path: '/api/user/{userId}',
+  //   method: 'GET',
+  //   options: {
+  //     cors: { origin: 'ignore' },
+  //     validate: {
+  //       params: Joi.object({
+  //         userId: Joi.string().required(),
+  //       }),
+  //     },
+  //   },
+  //   handler: async req => {
+  //     const { userId } = req.params;
+  //     try {
+  //       const user = await isValidToken(req.headers.authorization);
+  //       console.log(userId, user.uid);
+  //       if (userId !== user.uid) {
+  //         throw Boom.unauthorized();
+  //       }
+  //     } catch {
+  //       throw Boom.unauthorized();
+  //     }
 
-      const user = await db('user')
-        .select<{ id: string; isSubscribed: boolean; }>([
-          'user.id as id',
-          'user.isSubscribed as isSubscribed',
-        ])
-        .where('user.id', '=', userId)
-        .limit(1)
-        .first();
-      return user;
-    },
-  });
+  //     const user = await db('user')
+  //       .select<{ id: string; isSubscribed: boolean; }>([
+  //         'user.id as id',
+  //         'user.isSubscribed as isSubscribed',
+  //       ])
+  //       .where('user.id', '=', userId)
+  //       .limit(1)
+  //       .first();
+  //     return user;
+  //   },
+  // });
 
-  server.route({
-    path: '/api/user/{userId}',
-    method: 'POST',
-    options: {
-      cors: { origin: 'ignore' },
-      validate: {
-        params: Joi.object({
-          userId: Joi.string().required(),
-        }),
-        payload: Joi.object({
-          isSubscribed: Joi.boolean().required(),
-        }),
-      },
-    },
-    handler: async req => {
-      const { userId } = req.params;
-      try {
-        const user = await isValidToken(req.headers.authorization);
-        if (userId !== user.uid) {
-          throw Boom.unauthorized();
-        }
-      } catch {
-        throw Boom.unauthorized();
-      }
+  // server.route({
+  //   path: '/api/user/{userId}',
+  //   method: 'POST',
+  //   options: {
+  //     cors: { origin: 'ignore' },
+  //     validate: {
+  //       params: Joi.object({
+  //         userId: Joi.string().required(),
+  //       }),
+  //       payload: Joi.object({
+  //         isSubscribed: Joi.boolean().required(),
+  //       }),
+  //     },
+  //   },
+  //   handler: async req => {
+  //     const { userId } = req.params;
+  //     try {
+  //       const user = await isValidToken(req.headers.authorization);
+  //       if (userId !== user.uid) {
+  //         throw Boom.unauthorized();
+  //       }
+  //     } catch {
+  //       throw Boom.unauthorized();
+  //     }
 
-      const user = await db('user')
-        .update({
-          isSubscribed: (req.payload as any).isSubscribed,
-          updatedAt: db.fn.now(),
-        })
-        .where('user.id', '=', userId);
-      return user;
-    },
-  });
+  //     const user = await db('user')
+  //       .update({
+  //         isSubscribed: (req.payload as any).isSubscribed,
+  //         updatedAt: db.fn.now(),
+  //       })
+  //       .where('user.id', '=', userId);
+  //     return user;
+  //   },
+  // });
 }
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,5 @@
-import { Server } from '@hapi/hapi';
+import fastify from 'fastify'
+import fastifyCors from 'fastify-cors'
 import * as Sentry from '@sentry/node';
 
 import config from '../config';
@@ -8,14 +9,19 @@ const port = parseInt(process.env.PORT, 10) || 5000;
 Sentry.init({ dsn: config.dsn });
 
 (async () => {
-  const server = new Server({
-    debug: { request: ['implementation'] },
-    port,
-  });
+  const server = fastify({
+    logger: true
+  })
 
   // register api routes
   await registerApiRoutes(server);
 
-  await server.start();
-  console.log(`> Ready on http://localhost:${port}`);
+  server.register(fastifyCors, {
+    // put your options here
+  })
+
+  server.listen(port, (err, address) => {
+    if (err) throw err
+    server.log.info(`server listening on ${address}`)
+  })
 })();
