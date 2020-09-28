2ba49c68f294f17675438e46f53011cbe6ebb5f3
Scott Cooper <scttcper@gmail.com>
2020-09-27 20:24:04 -0700

    add a donate button

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => (
-  <div className="bg-white mx-auto my-3 pb-12 py-1 px-4 lg:px-8 rounded-lg prose pt-10">
+  <div className="bg-white mx-auto my-3 pb-12 py-1 px-4 md:px-6 lg:px-8 rounded-lg prose pt-10">
     <div className="prose">
       <h1>About</h1>
 
@@ -15,22 +15,33 @@ const About = () => (
         >
           XM Satellite Radio
         </a>{' '}
-        and organize them into spotify playlists.
+        to track the most played songs and organize them into spotify playlists.
       </p>
 
-      <p>
-        Contact us at <a href="mailto:hello@xmplaylist.com">hello@xmplaylist.com</a> or on twitter{' '}
-        <a href="https://twitter.com/scttcper">@scttcper</a>
-      </p>
+      <h3>Donate</h3>
+      <figure>
+        <a href="https://www.buymeacoffee.com/scooper" target="_blank" rel="noopener noreferrer">
+          <img
+            src="/static/img/default-orange.png"
+            alt="Buy Me A Coffee"
+            className="w-60"
+            style={{ margin: 0 }}
+          />
+        </a>
+        <figcaption>Support future development by buying me a coffee.</figcaption>
+      </figure>
 
+      <h3>Contact</h3>
       <p>
-        Make sure to join the nice people in the{' '}
+        Email us at <a href="mailto:hello@xmplaylist.com">hello@xmplaylist.com</a> or on twitter{' '}
+        <a href="https://twitter.com/scttcper">@scttcper</a>. Or join the nice people in the
+        SiriusXM Fans{' '}
         <a
           href="https://www.facebook.com/groups/siriusxmfans"
           rel="noopener noreferrer"
           target="_blank"
         >
-          SiriusXM Fans Facebook group
+          Facebook group
         </a>
       </p>
 
