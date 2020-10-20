55b1cfa27a88fc9e80f0f4f5a611a6bc071d1a41
Scott Cooper <scttcper@gmail.com>
2020-10-20 10:38:38 -0700

    reduce transactions

--- api/src/server.ts
+++ api/src/server.ts
@@ -16,7 +16,7 @@ import { registerApiRoutes } from './apiRoutes';
 Sentry.init({
   dsn: config.dsn,
   integrations: [new Sentry.Integrations.Http({ tracing: true })],
-  tracesSampleRate: 0.6,
+  tracesSampleRate: 0.1,
   environment: process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
 });
 
