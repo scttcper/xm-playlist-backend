abf32ee649aeef9c1ef11500dd9cb844d71f8693
Scott Cooper <scttcper@gmail.com>
2020-10-20 21:19:12 -0700

    undefined

--- frontend/components/SearchForm.tsx
+++ frontend/components/SearchForm.tsx
@@ -133,8 +133,8 @@ export const SearchForm = ({
               id="timeAgo"
               className="block form-select w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
               onChange={() => {
-                setValue('startDate', null);
-                setValue('endDate', null);
+                setValue('startDate', undefined);
+                setValue('endDate', undefined);
               }}
             >
               <option value="">Date Range</option>
