08d31ec88ce1b529dcec3876bedbb92b2887f467
Scott Cooper <scttcper@gmail.com>
2020-11-25 14:08:33 -0800

    drop headers

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,17 +5,4 @@ module.exports = withPreact({
   compress: false,
   poweredByHeader: false,
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/_next/image',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public, max-age=864000, immutable',
-          },
-        ],
-      },
-    ];
-  },
 });
