027f4fa2b9d26dc61f431fa8351ac6f26018d68a
Scott Cooper <scttcper@gmail.com>
2021-01-22 22:23:50 -0800

    fix station image

--- frontend/components/StationHeader.tsx
+++ frontend/components/StationHeader.tsx
@@ -17,15 +17,16 @@ export const StationHeader = ({ channel, currentPage }: Props) => {
   return (
     <>
       <div className="max-w-7xl mx-auto px-1 md:px-4 sm:px-6 lg:px-8 my-2 md:mt-4 md:mb-3 flex">
-        <div className="flex-0 bg-cool-gray-900 rounded-lg p-3 mr-2 md:mr-3 flex">
-          <Image
-            src={`/img/${channel.deeplink}-sm.png`}
-            className="w-24 h-24 md:h-32 md:w-32"
-            alt={`${channel.name} Logo`}
-            quality={100}
-            width={138}
-            height={138}
-          />
+        <div className="flex-0 mr-2 md:mr-3 flex w-24 md:w-32">
+          <div className="bg-cool-gray-900 rounded-lg p-3">
+            <Image
+              src={`/img/${channel.deeplink}-sm.png`}
+              alt={`${channel.name} Logo`}
+              quality={100}
+              width={138}
+              height={138}
+            />
+          </div>
         </div>
         <div className="flex-1 bg-white rounded-lg p-3 sm:p-4 pt-2 flex flex-col justify-between">
           <div className="flex-1">
@@ -38,8 +39,8 @@ export const StationHeader = ({ channel, currentPage }: Props) => {
               </Link>
             </h2>
             <div className="flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap">
-              <div className="md:mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
-                {channel.desc}
+              <div className="md:mt-2 flex items-center text-sm leading-5 text-gray-500 mb-2 sm:mr-6">
+                <p>{channel.desc}</p>
               </div>
             </div>
           </div>
