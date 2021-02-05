c74eae9f375d718015523ab0a8ff5acb0b3e6ed1
Scott Cooper <scttcper@gmail.com>
2021-02-04 22:22:43 -0800

    max width

--- frontend/pages/station/index.tsx
+++ frontend/pages/station/index.tsx
@@ -7,7 +7,7 @@ import { NeworAd } from 'components/Newor';
 const StationsPage: React.FC = () => (
   <>
     <div className="max-w-7xl mx-auto pb-3 px-1 md:px-4 sm:px-6 lg:px-8">
-      <NeworAd id="waldo-tag-9958" />
+      <NeworAd id="waldo-tag-9958" style={{ maxWidth: '970px' }} />
     </div>
     <div className="max-w-7xl mx-auto pb-12 px-1 md:px-4 sm:px-6 lg:px-8">
       <div className="bg-white my-3 pb-12 py-4 lg:py-8 px-4 lg:px-8 sm:rounded-lg">
