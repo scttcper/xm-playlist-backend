108bae9f8dde6374cfa220146d36bd19825c8782
Scott Cooper <scttcper@gmail.com>
2021-03-15 21:51:27 -0700

    switch to tailwind jit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  purge: ['./components/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
