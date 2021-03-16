de95d111753cf7baaf66192b923092e35e94b720
Scott Cooper <scttcper@gmail.com>
2021-03-15 22:06:13 -0700

    add donate button

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -87,6 +87,14 @@ export const NavBar: React.FC = () => {
                   Search
                 </a>
               </Link>
+              <a
+                href="https://www.buymeacoffee.com/scooper"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+              >
+                Donate
+              </a>
             </div>
           </div>
 
@@ -197,6 +205,14 @@ export const NavBar: React.FC = () => {
               Search
             </a>
           </Link>
+          <a
+            href="https://www.buymeacoffee.com/scooper"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Donate
+          </a>
         </div>
       </div>
     </nav>
